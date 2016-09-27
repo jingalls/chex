a5088f66d5d001c77f9b2ff4289235706429fbcc
Joe Ingalls <jingalls@accuvant.com>
2016-09-27 18:12:16 -0400

    Updated the initial form layout to allow for more control from the user as to what phantom assets are used. Modified the phantom_form.js file to work with the new form layouts data. Updated the css to work with the new form. Added a few bower components like noty, jquery and bootstrap.

--- js/phantom.js
+++ js/phantom.js
@@ -23,7 +23,7 @@ var phantom_connector = {
 
         var data = {
             description: 'Test Incident from Chrome Extension',
-            label: 'Incident',
+            label: 'CHEX',
             name: 'Test Incident from Chrome Extension'
         };
 
@@ -49,6 +49,40 @@ var phantom_connector = {
             url: url,
             type: 'POST',
             data: JSON.stringify(args.data),
+            dataType: 'json',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Authorization', 'Basic ' + btoa(args.username + ':' + args.password));
+            },
+            success: args.success,
+            error: args.error,
+            cache: args.cache || false
+        });
+    },
+    get_available_toolset: function(args) {
+        var url = args.host;
+        if (!url.endsWith('/')) { url += '/'; }
+        url += 'decided_list/chrome_extension_tools';
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Authorization', 'Basic ' + btoa(args.username + ':' + args.password));
+            },
+            success: args.success,
+            error: args.error,
+            cache: args.cache || false
+        });
+    },
+    get_available_formats: function(args) {
+        var url = args.host;
+        if (!url.endsWith('/')) { url += '/'; }
+        url += 'decided_list/chrome_extension_report_formats';
+
+        $.ajax({
+            url: url,
+            type: 'GET',
             dataType: 'json',
             beforeSend: function(xhr) {
                 xhr.setRequestHeader('Authorization', 'Basic ' + btoa(args.username + ':' + args.password));
